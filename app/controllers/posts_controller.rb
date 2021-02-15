7efadb60864c25f486f521c02f3fd258300048ca
gao7aokenichi <kendantyo@gmail.com>
2021-02-15 19:36:31 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,16 @@ class PostsController < ApplicationController
   #メモを保存した後にトップページへリダイレクトされる
   redirect_to action: :index
   end
+  #checkedというアクションを定義
+  def checked
+    post = Post.find(params[:id])
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
